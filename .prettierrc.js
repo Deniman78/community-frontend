9f6a56931ddf90ee2282e47cd679830a0d162e22
Deniman78 <deniman25@gmail.com>
2021-05-10 20:59:11 +0300

    added file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  endOfLine: 'crlf',
+  arrowParens: 'avoid',
+  semi: true,
+};
